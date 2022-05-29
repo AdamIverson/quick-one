3d61356d6618781d0082c54ae661870dd937b879
Adam Iverson <aciverso@gmail.com>
2022-05-29 13:03:57 -0500

    almost almost

--- client/src/components/Test/Test.jsx
+++ client/src/components/Test/Test.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Test() {
-  return (<h1>Sioux Falls</h1>)
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    callAPI();
+  }, []);
+
+  function callAPI() {
+    console.log('in callAPI()');
+    axios({
+      method: "GET",
+      url: "/api/test",
+    })
+      .then((response) => {
+        console.log('response.data:', response.data);
+        console.log('response.status:', response.status);
+        console.log('response.header:', response.header);
+        setImages(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log(err.response);
+        console.log(err.request);
+        // console.log(err.headers);
+      });
+  }
+
+  return <ul>
+    <li>GOLF</li>
+  </ul>;
 }
